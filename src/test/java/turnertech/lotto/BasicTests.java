a078614c639b317a61e87d647fc0cea33091cf62
Luke Turner <luke.ian.turner@outlook.de>
2022-11-23 18:27:16 +0100

    Add dummy tests for pipeline

---
+++ src/test/java/turnertech/lotto/BasicTests.java
@@ -0,0 +1,12 @@
+package turnertech.lotto;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class BasicTests {
+    @Test
+	public void dummyTest() {
+        assertTrue(true);
+    }
+}
